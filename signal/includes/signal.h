64e89546d594b0277b99074c6e4a9c51a3270c9f
kiri0314 <t.kirihara0314@gmail.com>
2022-02-12 01:35:49 +0900

    UPGRADE: ヘッダーをインクルード

--- signal/includes/signal.h
+++ signal/includes/signal.h
@@ -6,11 +6,15 @@
 /*   By: tkirihar <tkirihar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/11 23:34:51 by tkirihar          #+#    #+#             */
-/*   Updated: 2022/02/11 23:56:52 by tkirihar         ###   ########.fr       */
+/*   Updated: 2022/02/12 01:34:57 by tkirihar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SIGNAL_H
 # define SIGNAL_H
+
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
 
 #endif
