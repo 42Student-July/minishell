a3b33d5fa660b9b1f853edf9c4fdc61e773a8c83
kiri0314 <t.kirihara0314@gmail.com>
2022-02-12 01:47:30 +0900

    ADD: set_signal.cを追加

--- signal/includes/signal.h
+++ signal/includes/signal.h
@@ -6,7 +6,7 @@
 /*   By: tkirihar <tkirihar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/11 23:34:51 by tkirihar          #+#    #+#             */
-/*   Updated: 2022/02/12 01:34:57 by tkirihar         ###   ########.fr       */
+/*   Updated: 2022/02/12 01:47:22 by tkirihar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,11 @@
 # include <stdlib.h>
 # include <stdio.h>
 # include <string.h>
+
+// set_signal.c
+
+void	set_interactive_sigint(void);
+void	set_dfl_sigint(void);
+void	set_dfl_sigquit(void);
 
 #endif
---
+++ signal/src/set_signal.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   set_signal.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tkirihar <tkirihar@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/11 23:57:59 by tkirihar          #+#    #+#             */
+/*   Updated: 2022/02/12 00:05:52 by tkirihar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "signal/includes/signal.h"
+
+void	set_interactive_sigint(void)
+{
+	struct sigaction	act;
+
+	act.sa_sigaction = interactive_sigint;
+	act.sa_flags = 0;
+	sigemptyset(&act.sa_mask);
+	sigaction(SIGINT, &act, NULL);
+}
+
+void	set_dfl_sigint(void)
+{
+	struct sigaction	act;
+
+	act.sa_sigaction = dfl_sigint;
+	act.sa_flags = 0;
+	sigemptyset(&act.sa_mask);
+	sigaction(SIGINT, &act, NULL);
+}
+
+void	set_dfl_sigquit(void)
+{
+	struct sigaction	act;
+
+	act.sa_sigaction = dfl_sigquit;
+	act.sa_flags = 0;
+	sigemptyset(&act.sa_mask);
+	sigaction(SIGQUIT, &act, NULL);
+}
