080da9c23271e48357f8acdec9413926caceee48
bayamasa <mapi0213mgt@gmail.com>
2022-02-15 17:42:52 +0900

    WIP: echoのコマンドが正しく実行されるように実装

--- self_cmd/self_echo.c
+++ self_cmd/self_echo.c
@@ -6,14 +6,14 @@
 /*   By: mhirabay <mhirabay@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/07 13:35:12 by tkirihar          #+#    #+#             */
-/*   Updated: 2022/02/15 14:04:18 by mhirabay         ###   ########.fr       */
+/*   Updated: 2022/02/15 17:25:00 by mhirabay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "self_cmd.h"
 
-void	exec_self_echo(t_exec_attr *ea)
+void	exec_self_echo(t_cmd *cmd, t_exec_attr *ea)
 {
-	(void)ea;
+	(void)cmd;
 	ft_putstr_fd(get_cmd_arg(ea->cmd_lst, ea), STDOUT_FILENO);
 }
