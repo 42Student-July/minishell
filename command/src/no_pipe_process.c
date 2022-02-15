437c986bc659052faf5a83d715ef1479cb86815d
bayamasa <mapi0213mgt@gmail.com>
2022-02-15 10:31:00 +0900

    MOD: selfコマンドが実行できるように修正

--- command/src/no_pipe_process.c
+++ command/src/no_pipe_process.c
@@ -6,7 +6,7 @@
 /*   By: mhirabay <mhirabay@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/11 16:23:41 by tkirihar          #+#    #+#             */
-/*   Updated: 2022/02/14 23:33:56 by mhirabay         ###   ########.fr       */
+/*   Updated: 2022/02/15 10:20:55 by mhirabay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,13 @@ void	execute_ext_cmd(t_exec_attr *ea)
 
 	cmdv = convert_lst_to_argv(get_exec_cmd(ea)->args);
 	cmd_path = find_path(ea->cmd->content, ea);
-	environ = convert_envlst_to_array(ea->env_lst);
+	environ = convert_envlst_to_array(ea);
 	pid = fork();
 	if (pid == -1)
 		printf("fork error\n");
 	else if (pid == 0)
 	{
-		if (execve(cmd_path, cmdv, NULL) == -1)
+		if (execve(cmd_path, cmdv, environ) == -1)
 		{
 			printf("exec error\n");
 			exit(EXIT_FAILURE);
@@ -42,29 +42,10 @@ void	execute_ext_cmd(t_exec_attr *ea)
 	}
 }
 
-void	execute_self_cmd(t_exec_attr *ea)
-{
-	if (is_(CD, ea))
-		exec_self_cd(ea);
-	else if (is_(EXPORT, ea))
-		exec_self_export(ea);
-	else if (is_(EXIT, ea))
-		exec_self_exit(ea);
-	else if (is_(UNSET, ea))
-		exec_self_unset(ea);
-	else if (is_(PWD, ea))
-		exec_self_pwd(ea);
-	else if (is_(ECHO, ea))
-		exec_self_echo(ea);
-	else if (is_(ENV, ea))
-		exec_self_env(ea);
-}
-
-
 void	no_pipe_process(t_exec_attr *ea)
 {
-	if (is_self_cmd(get_cmd(ea->cmd)))
-		execute_self_cmd(ea);
+	if (is_self_cmd(get_cmd_name(ea->cmd)))
+		execute_self_cmd(ea->cmd, ea);
 	else
 		execute_ext_cmd(ea);
 }
