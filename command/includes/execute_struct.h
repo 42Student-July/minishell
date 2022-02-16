dd3efbaa34b6a70a5f81d2bac0ce2da0cdf81cf9
bayamasa <mapi0213mgt@gmail.com>
2022-02-16 10:17:19 +0900

    MOD: 必要なutil関数追加 後でリファクタする

--- command/includes/execute_struct.h
+++ command/includes/execute_struct.h
@@ -6,7 +6,7 @@
 /*   By: mhirabay <mhirabay@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/09 15:44:58 by mhirabay          #+#    #+#             */
-/*   Updated: 2022/02/15 14:19:45 by mhirabay         ###   ########.fr       */
+/*   Updated: 2022/02/16 09:53:06 by mhirabay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,6 +46,8 @@ t_cmd		*get_cmd(t_exec_attr *ea);
 char		*get_cmd_name(t_list *cmd);
 char		**get_args(t_list *cmd, t_exec_attr *ea);
 char		*get_cmd_arg(t_list *cmd, t_exec_attr *ea);
+char		*get_cmd_args(t_cmd *cmd, t_exec_attr *ea);
+char		*get_argv_one(t_cmd *cmd);
 
 #endif
 
--- command/src/exec_attribute_utils.c
+++ command/src/exec_attribute_utils.c
@@ -6,7 +6,7 @@
 /*   By: mhirabay <mhirabay@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/14 18:31:09 by mhirabay          #+#    #+#             */
-/*   Updated: 2022/02/15 14:09:55 by mhirabay         ###   ########.fr       */
+/*   Updated: 2022/02/16 09:52:12 by mhirabay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,4 +44,37 @@ char	*get_cmd_arg(t_list *cmd_lst, t_exec_attr *ea)
 
 	args = get_args(cmd_lst, ea);
 	return (args[CMD_ARG]);
+}
+
+char	**get_args_new(t_cmd *cmd, t_exec_attr *ea)
+{
+	char	**args;
+
+	args = convert_lst_to_argv(cmd->args);
+	if (args == NULL)
+		abort_minishell(MALLOC_ERROR, ea);
+	return (args);
+}
+
+char	*get_cmd_args(t_cmd *cmd, t_exec_attr *ea)
+{
+	char	**args;
+
+	args = get_args_new(cmd, ea);
+	return (args[CMD_ARG]);
+}
+
+char	*get_argv_one(t_cmd *cmd)
+{
+	if (cmd->args == NULL)
+	{
+		ft_putstr_fd("no command", STDERR_FILENO);
+		return (NULL);
+	}
+	if (cmd->args->next == NULL)
+	{
+		ft_putstr_fd("no args", STDERR_FILENO);
+		return (NULL);
+	}
+	return (cmd->args->next->content);
 }
