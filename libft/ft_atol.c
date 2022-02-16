2edb0dde51342d6def6cfcda26d7c4022e18d04a
kiri0314 <t.kirihara0314@gmail.com>
2022-02-17 01:04:48 +0900

    UPGRADE: ft_atol関数を追加

--- libft/ft_atol.c
+++ libft/ft_atol.c
@@ -1,14 +1,39 @@
 #include "libft.h"
 #include <stdio.h>
 
+static long long	ft_atoll(const char *str)
+{
+	size_t		i;
+	long long	num;
+	int			sign;
+
+	num = 0;
+	i = 0;
+	sign = 1;
+	if (!str)
+		return (0);
+	while (ft_isspace(str[i]))
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		num = (num * 10) + (str[i] - '0');
+		i++;
+	}
+	return (num * sign);
+}
+
 long	ft_atol(const char *str)
 {
-	printf("ft_atol\n");
-	return (__LONG_MAX__);
-	// long	num;
+	long long	num;
 
-	// num = ft_long_atoi(str);
-	// return (INT_MIN <= num && num <= INT_MAX);
+	num = ft_atoll(str);
+	return (num);
 }
 
 int main()
