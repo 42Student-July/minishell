5f35aefbe77415cff94586eedb5ae266773978be
bayamasa <mapi0213mgt@gmail.com>
2022-02-17 16:55:03 +0900

    FIX: updateのときに文字化けしてしまうバグを修正

--- environ/lst_utils_2.c
+++ environ/lst_utils_2.c
@@ -6,7 +6,7 @@
 /*   By: mhirabay <mhirabay@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/17 15:44:59 by mhirabay          #+#    #+#             */
-/*   Updated: 2022/02/17 16:00:29 by mhirabay         ###   ########.fr       */
+/*   Updated: 2022/02/17 16:51:26 by mhirabay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 bool	update_value(t_list *lst, char *key, char *new_v, t_exec_attr *ea)
 {
 	t_list	*target;
-	char	*current_v;
+	char	*tmp;
 
 	target = get_list_by_key(lst, key);
 	if (target == NULL)
@@ -23,17 +23,9 @@ bool	update_value(t_list *lst, char *key, char *new_v, t_exec_attr *ea)
 		printf("invalid key\n");
 		return (false);
 	}
-	current_v = ft_kvsget_value(target->content);
-
-	if (current_v == new_v)
-	{
-		// TODO: あとでけす
-		printf("new value equals current value\n");
-		return (true);
-	}
-	free(current_v);
-	current_v = ft_strdup(new_v);
-	if (current_v == NULL)
+	tmp = ft_strdup(new_v);
+	if (tmp == NULL)
 		abort_minishell(MALLOC_ERROR, ea);
+	ft_kvsreplace_value(target->content, tmp);
 	return (true);
 }
