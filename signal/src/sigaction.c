a3e28d5a6934fc2c3890e71a4670d80c1cacf194
kiri0314 <t.kirihara0314@gmail.com>
2022-02-17 23:14:02 +0900

    UPGRADE: interactive_sigintで終了ステータス1を追加

--- signal/src/sigaction.c
+++ signal/src/sigaction.c
@@ -6,7 +6,7 @@
 /*   By: tkirihar <tkirihar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/12 14:18:41 by tkirihar          #+#    #+#             */
-/*   Updated: 2022/02/13 01:23:55 by tkirihar         ###   ########.fr       */
+/*   Updated: 2022/02/17 22:53:32 by tkirihar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,10 @@
 
 int g_exit_status = 0;
 
-void interactive_sigint(int sig)
+void	interactive_sigint(int sig)
 {
 	(void)sig;
+	g_exit_status = 1;
 	printf("\n");
 	rl_replace_line("", 0); // プロンプトのバッファをクリア
 	rl_on_new_line();       // プロンプトを次の行に移動したいことを伝える？
