0c50478782eb151450d851d2c424c49e0e852aa0
bayamasa <mapi0213mgt@gmail.com>
2022-02-19 15:54:01 +0900

    MOD: oldpwdを以前の環境変数PWDからもってくるように修正

--- self_cmd/self_cd.c
+++ self_cmd/self_cd.c
@@ -6,44 +6,35 @@
 /*   By: mhirabay <mhirabay@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/02 14:54:54 by tkirihar          #+#    #+#             */
-/*   Updated: 2022/02/19 15:42:12 by mhirabay         ###   ########.fr       */
+/*   Updated: 2022/02/19 15:51:42 by mhirabay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "self_cmd.h"
 
-void	update_all_environ(char *pwd, char *oldpwd, t_exec_attr *ea)
+void	update_all_environ(char *pwd, t_exec_attr *ea)
 {
 	char	*export_pwd;
 	char	*export_oldpwd;
+	char	*old_pwd;
 
-	update_value(ea->env_lst, "OLDPWD", oldpwd, ea);
+	// OLDPWDは前のPWDからパクって来る
+	old_pwd = ft_kvsget_value(get_list_by_key(ea->env_lst, "PWD")->content);
+	export_oldpwd = ft_kvsget_value(get_list_by_key(ea->export_lst, "PWD")->content);
+	update_value(ea->env_lst, "OLDPWD", old_pwd, ea);
 	update_value(ea->env_lst, "PWD", pwd, ea);
 	export_pwd = create_export_value(pwd);
 	if (export_pwd == NULL)
 		abort_minishell(MALLOC_ERROR, ea);
-	export_oldpwd = create_export_value(oldpwd);
-	if (export_oldpwd == NULL)
-	{
-		free(export_pwd);
-		abort_minishell(MALLOC_ERROR, ea);
-	}
-	update_value(ea->export_lst, "OLDPWD", oldpwd, ea);
+	update_value(ea->export_lst, "OLDPWD", export_oldpwd, ea);
 	update_value(ea->export_lst, "PWD", pwd, ea);
 }
 
 int	x_chdir(char *path, t_exec_attr *ea)
 {
-	char	old_pwd[PATH_MAX];
 	char	pwd[PATH_MAX];
 
 	redirect_dev_null(ea);
-	if (getcwd(old_pwd, PATH_MAX) == NULL)
-	{
-		revert_redirect_out(ea);
-		print_error(PWD, path);
-		return (1);
-	}
 	if (chdir(path) == -1)
 	{		
 		revert_redirect_out(ea);
@@ -59,7 +50,7 @@ int	x_chdir(char *path, t_exec_attr *ea)
 	revert_redirect_out(ea);
 	// printf("old_pwd : %s\n", old_pwd);
 	// printf("pwd : %s\n", pwd);
-	update_all_environ(pwd, old_pwd, ea);
+	update_all_environ(pwd, ea);
 	return (0);
 }
 
