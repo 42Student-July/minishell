c0080261fa7becdd57cff8b35145d7c6277768ef
akito <akito@student.42tokyo.jp>
2022-02-21 17:43:29 +0900

    fix: echoで改行されない問題を修正

--- self_cmd/self_echo.c
+++ self_cmd/self_echo.c
@@ -10,17 +10,60 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 #include "self_cmd.h"
+#include <stdbool.h>
 
 int	exec_self_echo(t_cmd *cmd, t_exec_attr *ea)
 {
-	char	*argv_one;
+	t_list	*args;
+	char	*str;
+	int		i;
+	bool display_return ;
 
-	argv_one = get_argv_one(cmd);
-	if (argv_one == NULL)
-		return (1);
+	args = cmd->args->next;
 	(void)ea;
+	display_return = false;
 	// TODO: pipeでつないだときへの対応、引数が2つの場合にも対応
-	ft_putstr_fd(argv_one, STDOUT_FILENO);
+	while (args != NULL)
+	{
+		str = args->content;
+		if (str == NULL || str[0] != '-')
+			break ;
+		i = 1;
+		while (str[i])
+		{
+			if (ft_strchr("n", str[i]) == NULL)
+			{
+				ft_putstr_fd("echo: -", STDERR_FILENO);
+				ft_putchar_fd(str[i], STDERR_FILENO);
+				ft_putstr_fd(": invalid option\n", STDERR_FILENO);
+				return (1);
+			}
+			i++;
+		}
+		if (str[1] == '\0' || str[i] != '\0')
+			break ;
+		i = 1;
+		while (str[i] != '\0')
+		{
+			if (str[i] == 'n')
+				display_return = true;
+			i++;
+		}
+		args = args->next;
+	}
+	while (args != NULL)
+	{
+		str = args->content;
+		if (str == NULL)
+			break ;
+		args = args->next;
+		ft_putstr_fd(str, STDOUT_FILENO);
+		if (args != NULL)
+			ft_putchar_fd(' ', STDOUT_FILENO);
+		else if (!display_return)
+			ft_putchar_fd('\n', STDOUT_FILENO);
+	}
 	return (0);
 }
