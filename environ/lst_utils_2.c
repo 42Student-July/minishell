9397a6c616e081c1dfb60154fe0a84c475040ee1
bayamasa <mapi0213mgt@gmail.com>
2022-02-23 17:27:35 +0900

    MOD: もしvalueがnullだったら、値を更新しない実装を追加

--- environ/lst_utils_2.c
+++ environ/lst_utils_2.c
@@ -6,7 +6,7 @@
 /*   By: mhirabay <mhirabay@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/17 15:44:59 by mhirabay          #+#    #+#             */
-/*   Updated: 2022/02/19 15:57:07 by mhirabay         ###   ########.fr       */
+/*   Updated: 2022/02/23 17:00:36 by mhirabay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,9 @@ bool	update_value(t_list *lst, char *key, char *new_v, t_exec_attr *ea)
 	t_list	*target;
 	char	*tmp;
 
+	if (new_v == NULL)
+		// もしnewがnullだったら値は更新しない。
+		return (true);
 	target = get_lst_by_key(lst, key);
 	if (target == NULL)
 	{
