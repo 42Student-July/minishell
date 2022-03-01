455b38df2eb41731d8fbad355c0ed79fd2c70b0a
bayamasa <mapi0213mgt@gmail.com>
2022-03-01 10:33:07 +0900

    MOD: unsetを複数の引数に対応

--- self_cmd/self_unset.c
+++ self_cmd/self_unset.c
@@ -6,7 +6,7 @@
 /*   By: mhirabay <mhirabay@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/08 09:19:55 by mhirabay          #+#    #+#             */
-/*   Updated: 2022/02/24 17:07:14 by mhirabay         ###   ########.fr       */
+/*   Updated: 2022/03/01 10:18:44 by mhirabay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,26 @@
 
 int	exec_self_unset(t_cmd *cmd, t_exec_attr *ea)
 {
-	char	*argv_one;
+	t_list	*lst;
+	char	*arg;
 
-	argv_one = get_argv_one(cmd);
-	(void)ea;
-	if (argv_one == NULL)
-		return (1);
-	if (is_invalid_name(argv_one))
-		print_error_msg_with_var(UNSET, argv_one);
-	del_lst_by_key(ea->env_lst, argv_one);
-	del_lst_by_key(ea->export_lst, argv_one);
-	if (is_same_str(argv_one, "PWD"))
+	lst = cmd->args->next;
+	while (lst != NULL)
 	{
-		ea->current_pwd = ft_strdup("");
-		if (ea->current_pwd == NULL)
-			abort_minishell(MALLOC_ERROR, ea);
+		arg = (char *)(lst->content);
+		if (arg == NULL)
+			return (1);
+		if (is_invalid_name(arg))
+			print_error_msg_with_var(UNSET, arg);
+		del_lst_by_key(ea->env_lst, arg);
+		del_lst_by_key(ea->export_lst, arg);
+		if (is_same_str(arg, "PWD"))
+		{
+			ea->current_pwd = ft_strdup("");
+			if (ea->current_pwd == NULL)
+				abort_minishell(MALLOC_ERROR, ea);
+		}
+		lst = lst->next;
 	}
 	return (0);
 }
