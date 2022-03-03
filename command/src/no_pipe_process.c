2b0eee0021a55ca9b8d5ef11e64b43f94bd2b3c1
kiri0314 <t.kirihara0314@gmail.com>
2022-03-03 15:48:56 +0900

    upgrade: コマンドのパーミッションを確認する処理を追加

--- command/src/no_pipe_process.c
+++ command/src/no_pipe_process.c
@@ -6,7 +6,7 @@
 /*   By: tkirihar <tkirihar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/11 16:23:41 by tkirihar          #+#    #+#             */
-/*   Updated: 2022/03/02 17:25:04 by tkirihar         ###   ########.fr       */
+/*   Updated: 2022/03/03 15:28:35 by tkirihar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,15 +27,35 @@ bool	is_path(char *cmd)
 	return (false);
 }
 
+bool	can_exit(char *cmd_path)
+{
+	struct stat	stat_buf;
+
+	if (stat(cmd_path, &stat_buf) == -1)
+		exit(EXIT_FAILURE);
+	// 所有者の実行許可を確認している
+	if ((stat_buf.st_mode & S_IXUSR) != S_IXUSR)
+		return (false);
+	// 所有者の読み込み許可を確認している
+	if ((stat_buf.st_mode & S_IRUSR) != S_IRUSR)
+		return (false);
+	return (true);
+}
+
 void	error_process(int cp_errno, char *cmd_path)
 {
 	int	exit_status;
 
-	exit_status = EXIT_FAILURE;
+	// printf("errno %d\n", cp_errno);
 	if (cp_errno == ENOENT)
 		exit_status = 127;
-	else if (cp_errno == EACCES || cp_errno == ENOTDIR)
-		exit_status = 126;
+	else
+		exit_status = 126; // command not found以外は終了ステータスが126
+	// errnoがENOEXECでも実行権限がなければEACCESに変更する
+	if (cp_errno == ENOEXEC && !can_exit(cmd_path))
+		cp_errno = EACCES;
+	else if (cp_errno == ENOEXEC)
+		exit(EXIT_SUCCESS);
 	ft_put_error(strerror(cp_errno), cmd_path);
 	exit(exit_status);
 }
@@ -74,6 +94,7 @@ void	execute_ext_cmd(t_cmd *c, t_exec_attr *ea)
 		cmd_path = find_path(c->cmd, ea);
 		if (cmd_path == NULL)
 		{
+			printf("c->cmd [%s]\n", c->cmd);
 			ft_put_error("command not found", c->cmd);
 			g_exit_status = 127;
 			return ;
