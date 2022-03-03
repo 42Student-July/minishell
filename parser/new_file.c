2edcbef134bd427f414ebc31477d79173d0ad5ab
akito <akito@student.42tokyo.jp>
2022-03-03 21:51:36 +0900

    feat:  のような場合、t_file構造体のfileにNULLが入るようにした

--- parser/new_file.c
+++ parser/new_file.c
@@ -1,19 +1,23 @@
-#include "parser.h"
-#include "libft.h"
 #include "lexer.h"
+#include "libft.h"
+#include "parser.h"
 #include <stdbool.h>
 
-t_file *new_file(char *filename, bool is_double)
+t_file	*new_file(char *filename, bool is_double)
 {
-	t_file *file;
+	t_file	*file;
 
 	file = (t_file *)ft_calloc(sizeof(t_file), 1);
 	if (file == NULL)
 		exit(EXIT_FAILURE);
-	file->filename = ft_strdup(filename);
-	if (file->filename == NULL)
-		exit(EXIT_FAILURE);
+	if (filename != NULL)
+	{
+		file->filename = ft_strdup(filename);
+		if (file->filename == NULL)
+			exit(EXIT_FAILURE);
+	}
+	else
+		file->filename = NULL;
 	file->is_double = is_double;
 	return (file);
 }
-
