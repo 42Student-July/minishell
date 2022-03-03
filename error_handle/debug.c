24bc2dfcd30715f31d42e253ed4e0791b22198b1
bayamasa <mapi0213mgt@gmail.com>
2022-03-03 17:11:08 +0900

    MOD: 不要な関数を削除

--- error_handle/debug.c
+++ error_handle/debug.c
@@ -6,24 +6,11 @@
 /*   By: mhirabay <mhirabay@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 13:31:50 by mhirabay          #+#    #+#             */
-/*   Updated: 2022/02/15 13:32:52 by mhirabay         ###   ########.fr       */
+/*   Updated: 2022/03/03 17:10:38 by mhirabay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "error_handle.h"
-
-void	print_command_debug(t_exec_attr *ea)
-{
-	// size_t	i;
-
-	// i = 0;
-	(void)ea;
-	// while (ea->command[i] != NULL)
-	// {
-	// 	printf("ea->command[i] : %s\n", ea->command[i]);
-	// 	i++;
-	// }
-}
 
 void	print_array(char **array)
 {
