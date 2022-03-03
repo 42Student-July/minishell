5ffb6d50aa51801cf5c11121b64c2f65a25a2fb1
bayamasa <mapi0213mgt@gmail.com>
2022-03-03 16:33:51 +0900

    MOD: pipe用のopen関数を導入

--- command/src/pipe_process.c
+++ command/src/pipe_process.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pipe_process.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tkirihar <tkirihar@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: mhirabay <mhirabay@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/10 21:07:42 by tkirihar          #+#    #+#             */
-/*   Updated: 2022/02/23 22:48:44 by tkirihar         ###   ########.fr       */
+/*   Updated: 2022/03/03 16:33:24 by mhirabay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -134,6 +134,8 @@ void	exec_cmd(t_exec_attr *ea, t_pipe_attr *pa)
 	}
 	else if (pid == 0)
 	{
+		if (has_redirect_file(pa->current_cmd))
+			open_files(pa->current_cmd, ea);
 		set_pipe_fd(pa);
 		if (!is_self_cmd(pa->current_cmd->cmd))
 		{
