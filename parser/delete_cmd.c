3fd866773c4a46db50f0494f10553af2c8767463
Akihide Ito <39990441+AkkyOrz@users.noreply.github.com>
2022-03-04 20:56:15 +0900

    lexer関連でうまくdeleteできていなかったインスタンスの memory leak の修正 (#122)
    * fix: freeの順序がおかしい箇所を修正
    
    * fix: file_in, file_outをfreeするときの順序がおかしい箇所を修正
    
    * change tester

--- parser/delete_cmd.c
+++ parser/delete_cmd.c
@@ -9,9 +9,9 @@ void	delete_cmd(t_cmd **cmd)
 	ft_lstclear(&(*cmd)->args, free);
 	(*cmd)->args = NULL;
 	ft_lstclear(&(*cmd)->filenames_in, delete_file);
-	(*cmd)->filenames_out = NULL;
-	ft_lstclear(&(*cmd)->filenames_out, delete_file);
 	(*cmd)->filenames_in = NULL;
+	ft_lstclear(&(*cmd)->filenames_out, delete_file);
+	(*cmd)->filenames_out = NULL;
 	free(*cmd);
 	(*cmd) = NULL;
 }
