e599d5c64ab5550fd6415a8750a82d6a88044dbb
bayamasa <mapi0213mgt@gmail.com>
2022-03-05 10:57:50 +0900

    MOD: valueがNULLの値も許容する

--- command/src/execute_builtin.c
+++ command/src/execute_builtin.c
@@ -6,7 +6,7 @@
 /*   By: mhirabay <mhirabay@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/02 11:07:13 by tkirihar          #+#    #+#             */
-/*   Updated: 2022/02/18 17:45:29 by mhirabay         ###   ########.fr       */
+/*   Updated: 2022/03/05 10:56:40 by mhirabay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,27 @@ bool	is_not_exec_path(const char *command)
 	return (true);
 }
 
+char	*create_env_line_non_value(char *key, bool is_end)
+{
+	size_t	key_size;
+	size_t	line_size;
+	char	*line;
+
+	key_size = ft_strlen(key);
+	line_size = key_size + EQUAL + LF;
+	line = (char *)ft_calloc(sizeof(char), line_size);
+	if (line == NULL)
+		return (NULL);
+	ft_strlcat(line, key, line_size);
+	ft_strlcat(line, "=", line_size);
+	if (is_end)
+		ft_strlcat(line, "\n", line_size);
+	else
+		ft_strlcat(line, "\0", line_size);
+	return (line);
+}
+
+
 char	**convert_envlst_to_array(t_exec_attr *ea)
 {
 	char	**array;
@@ -43,15 +64,25 @@ char	**convert_envlst_to_array(t_exec_attr *ea)
 	{
 		if (ft_kvsget_value(tmp->content) == NULL)
 		{
-			i++;
-			tmp = tmp->next;
-			continue ;
+			array[i] = create_env_line_non_value(ft_kvsget_key(tmp->content), false);
 		}
-		//　TODO: 行数長くなっちゃうからget_keyの引数はlstでもいいのかも
-		array[i] = create_environ_line(\
-			ft_kvsget_key(tmp->content), ft_kvsget_value(tmp->content), false);
-		if (array[i] == NULL)
-			abort_minishell_with(MALLOC_ERROR, ea, array);
+		else
+		{
+			array[i] = create_environ_line(\
+				ft_kvsget_key(tmp->content), ft_kvsget_value(tmp->content), false);
+		}
+		if (i == env_lst_size - 1)
+		{
+			if (ft_kvsget_value(tmp->content) == NULL)
+			{
+				array[i] = create_env_line_non_value(ft_kvsget_key(tmp->content), true);
+			}
+			else
+			{
+				array[i] = create_environ_line(\
+					ft_kvsget_key(tmp->content), ft_kvsget_value(tmp->content), true);
+			}
+		}
 		tmp = tmp->next;
 		i++;
 	}
