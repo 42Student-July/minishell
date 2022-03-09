ad283c7ec9289863b2405c0e98eeef319da0151e
astalow <straysheep_1727@yahoo.co.jp>
2022-03-09 17:36:17 +0900

    fix:関数を分割

--- lexer/filter_null_literal_token.c
+++ lexer/filter_null_literal_token.c
@@ -12,29 +12,30 @@ static void	skip_null_literal(t_token **token, t_list **lst)
 	}
 }
 
-static void
-	copy_except_null(t_list **now, t_list **res, t_token **token, t_list **lst)
+static void	copy_except_null(t_token **token, t_list **now, t_list **lst, t_list **res)
 {
-	t_list	*tmp;
+	t_list *tmp;
 
 	while ((*lst)->next != NULL)
 	{
-		token = (*lst)->next->content;
+		*token = (*lst)->next->content;
 		if (((*token)->type == TOKEN_IDENT && (*token)->literal != NULL)
 			|| (*token)->type != TOKEN_IDENT)
 		{
 			tmp = ft_lstnew(token_dup(*token));
 			if (tmp == NULL)
 			{
-				ft_lstclear(res, delete_token);
-				break ;
+				ft_lstclear(&(*res), delete_token);
+				return ;
 			}
 			(*now)->next = tmp;
 			(*now) = (*now)->next;
 			(*lst) = (*lst)->next;
 		}
 		else
+		{
 			(*lst) = (*lst)->next;
+		}
 	}
 }
 
@@ -51,6 +52,6 @@ t_list	*filter_null_literal_token(t_list *lst)
 	if (now == NULL)
 		return (NULL);
 	res = now;
-	copy_except_null(&now, &res, &token, &lst);
+	copy_except_null(&token, &now, &lst, &res);
 	return (res);
 }
