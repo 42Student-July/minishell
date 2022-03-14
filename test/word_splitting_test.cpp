229268ad142adcdbe23ad7a5a1c3f139a3dfc62e
Akihide Ito <39990441+AkkyOrz@users.noreply.github.com>
2022-03-14 18:39:43 +0900

    fix: word splitting でambiguous rediret となるケースの存在 (#179)

--- test/word_splitting_test.cpp
+++ test/word_splitting_test.cpp
@@ -87,7 +87,6 @@ TEST(word_split, quote)
 		{TOKEN_EOF, ""},
 	};
 	word_split(input);
-	print_tokens(input);
 	compare_tokens(input, expected_vector);
 	ft_lstclear(&input, &delete_token);
 }
@@ -108,7 +107,6 @@ TEST(word_split, quoted_space)
 		{TOKEN_EOF, ""},
 	};
 	word_split(input);
-	print_tokens(input);
 	compare_tokens(input, expected_vector);
 	ft_lstclear(&input, &delete_token);
 }
@@ -130,7 +128,6 @@ TEST(word_split, operators)
 		{TOKEN_EOF, ""},
 	};
 	word_split(input);
-	print_tokens(input);
 	compare_tokens(input, expected_vector);
 	ft_lstclear(&input, &delete_token);
 }
@@ -152,7 +149,154 @@ TEST(word_split, echo)
 		{TOKEN_EOF, ""},
 	};
 	word_split(input);
-	print_tokens(input);
+	compare_tokens(input, expected_vector);
+	ft_lstclear(&input, &delete_token);
+}
+
+TEST(word_split, redirect_default)
+{
+	std::vector<std::pair<t_tokentype, std::string>> input_vector = {
+		{TOKEN_IDENT, "echo"},
+		{TOKEN_REDIRECT_OUT, ">"},
+		{TOKEN_IDENT, "\"a\""},
+		{TOKEN_EOF, ""},
+	};
+	t_list *input = init_input_lists(input_vector);
+
+	std::vector<std::pair<t_tokentype, std::string>> expected_vector = {
+		{TOKEN_IDENT, "echo"},
+		{TOKEN_REDIRECT_OUT, ">"},
+		{TOKEN_IDENT, "\"a\""},
+		{TOKEN_EOF, ""},
+	};
+	word_split(input);
+	compare_tokens(input, expected_vector);
+	ft_lstclear(&input, &delete_token);
+}
+
+TEST(word_split, redirect_ambiguous_multi)
+{
+	std::vector<std::pair<t_tokentype, std::string>> input_vector = {
+		{TOKEN_IDENT, "echo"},
+		{TOKEN_REDIRECT_OUT, ">"},
+		{TOKEN_IDENT, "\"a\" b"},
+		{TOKEN_EOF, ""},
+	};
+	t_list *input = init_input_lists(input_vector);
+
+	std::vector<std::pair<t_tokentype, std::string>> expected_vector = {
+		{TOKEN_IDENT, "echo"},
+		{TOKEN_REDIRECT_OUT, ">"},
+		{TOKEN_IDENT, "NULL"},
+		{TOKEN_EOF, ""},
+	};
+	word_split(input);
+	compare_tokens(input, expected_vector);
+	ft_lstclear(&input, &delete_token);
+}
+
+TEST(word_split, redirect_ambiguous_null)
+{
+	std::vector<std::pair<t_tokentype, std::string>> input_vector = {
+		{TOKEN_IDENT, "echo"},
+		{TOKEN_REDIRECT_OUT, ">"},
+		{TOKEN_IDENT, "  "},
+		{TOKEN_EOF, ""},
+	};
+	t_list *input = init_input_lists(input_vector);
+
+	std::vector<std::pair<t_tokentype, std::string>> expected_vector = {
+		{TOKEN_IDENT, "echo"},
+		{TOKEN_REDIRECT_OUT, ">"},
+		{TOKEN_IDENT, "NULL"},
+		{TOKEN_EOF, ""},
+	};
+	word_split(input);
+	compare_tokens(input, expected_vector);
+	ft_lstclear(&input, &delete_token);
+}
+
+TEST(word_split, redirect_only_one)
+{
+	std::vector<std::pair<t_tokentype, std::string>> input_vector = {
+		{TOKEN_IDENT, "echo"},
+		{TOKEN_REDIRECT_OUT, ">"},
+		{TOKEN_IDENT, "  test  "},
+		{TOKEN_EOF, ""},
+	};
+	t_list *input = init_input_lists(input_vector);
+
+	std::vector<std::pair<t_tokentype, std::string>> expected_vector = {
+		{TOKEN_IDENT, "echo"},
+		{TOKEN_REDIRECT_OUT, ">"},
+		{TOKEN_IDENT, "test"},
+		{TOKEN_IDENT, "NULL"},
+		{TOKEN_EOF, ""},
+	};
+	word_split(input);
+	compare_tokens(input, expected_vector);
+	ft_lstclear(&input, &delete_token);
+}
+
+TEST(word_split, redirect_only_one_quoted)
+{
+	std::vector<std::pair<t_tokentype, std::string>> input_vector = {
+		{TOKEN_IDENT, "echo"},
+		{TOKEN_REDIRECT_OUT, ">"},
+		{TOKEN_IDENT, "'  test  '"},
+		{TOKEN_EOF, ""},
+	};
+	t_list *input = init_input_lists(input_vector);
+
+	std::vector<std::pair<t_tokentype, std::string>> expected_vector = {
+		{TOKEN_IDENT, "echo"},
+		{TOKEN_REDIRECT_OUT, ">"},
+		{TOKEN_IDENT, "'  test  '"},
+		{TOKEN_EOF, ""},
+	};
+	word_split(input);
+	compare_tokens(input, expected_vector);
+	ft_lstclear(&input, &delete_token);
+}
+
+TEST(word_split, redirect_only_one_quoted_multi)
+{
+	std::vector<std::pair<t_tokentype, std::string>> input_vector = {
+		{TOKEN_IDENT, "echo"},
+		{TOKEN_REDIRECT_OUT, ">"},
+		{TOKEN_IDENT, "'  test  'hoge' 'fuga"},
+		{TOKEN_EOF, ""},
+	};
+	t_list *input = init_input_lists(input_vector);
+
+	std::vector<std::pair<t_tokentype, std::string>> expected_vector = {
+		{TOKEN_IDENT, "echo"},
+		{TOKEN_REDIRECT_OUT, ">"},
+		{TOKEN_IDENT, "'  test  'hoge' 'fuga"},
+		{TOKEN_EOF, ""},
+	};
+	word_split(input);
+	compare_tokens(input, expected_vector);
+	ft_lstclear(&input, &delete_token);
+}
+
+TEST(word_split, redirect_only_multi)
+{
+	std::vector<std::pair<t_tokentype, std::string>> input_vector = {
+		{TOKEN_IDENT, "echo"},
+		{TOKEN_REDIRECT_OUT, ">"},
+		{TOKEN_IDENT, "  test  ' hoge 'fuga"},
+		{TOKEN_EOF, ""},
+	};
+	t_list *input = init_input_lists(input_vector);
+
+	std::vector<std::pair<t_tokentype, std::string>> expected_vector = {
+		{TOKEN_IDENT, "echo"},
+		{TOKEN_REDIRECT_OUT, ">"},
+		{TOKEN_IDENT, "NULL"},
+		{TOKEN_EOF, ""},
+	};
+	word_split(input);
 	compare_tokens(input, expected_vector);
 	ft_lstclear(&input, &delete_token);
 }
